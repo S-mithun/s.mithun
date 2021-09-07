ce3415a4c774fa2489100b7e02b16c48c9eb3a5f
S-mithun <90100784+S-mithun@users.noreply.github.com>
2021-09-07 10:13:11 +0530

    datatypes

---
+++ Datatypes.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String args[]) {
+        int a=10;
+        float b=25.67867f;
+        double c=893;
+        char d='T';
+        String e="Java" ;
+        
+        System.out.println("int is"+a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+} 
+
